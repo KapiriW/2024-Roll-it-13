135decde07679794ae9a01f9736d98cd911e7c80
KapiriW <143568240+KapiriW@users.noreply.github.com>
2024-02-13 09:38:51 +1300

    Basic instructions and simple yes / no function

---
+++ C_01_Instructions.py
@@ -0,0 +1,18 @@
+print("🎲🎲 Roll it 13 🎲🎲")
+
+
+#loop for testing
+
+while True:
+    want_instructions = input("Do you want to read the instructions?").lower()
+
+    if want_instructions == "yes":
+        print("you chose yes")
+
+    # checks user enter y or n
+    elif want_instructions == "no":
+         print("you chose no")
+    elif want_instructions == "no":
+        print("you chose no")
+    else:
+        print("you did not chose a valid response")
