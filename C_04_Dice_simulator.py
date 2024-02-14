6fda76952954c05f4d303870390c7c42f4d26720
KapiriW <143568240+KapiriW@users.noreply.github.com>
2024-02-15 12:26:30 +1300

    it has 2 dice rolls which is added up to points

--- C_04_Dice_simulator.py
+++ C_04_Dice_simulator.py
@@ -6,6 +6,30 @@ import random
 def roll_die():
     result = random.randint( 1, 6)
     return result
+
+
 # main routine goes here
-die_roll = roll_die()
-print(die_roll)
+
+for item in range( 0, 10):
+    user_score = 0
+    double_score = "no"
+
+
+    # roll two dice
+    roll_1 = roll_die()
+    roll_2 = roll_die()
+
+    # check if we have a double score opportunity
+    if roll_1 == roll_2:
+        double_score
+
+    #find the total points (so far)
+    user_points = roll_1 + roll_2
+
+    # Show the user the result
+    print(f"Die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+    print(f"Double score opportunity: {double_score}")
+
+
+    die_roll = roll_die()
+    print(die_roll)
